f20aba45ebc6497f0e93c30891984e2bfcb9bd9b
suboo <subo.ttn.com>
2020-02-25 15:08:47 +0530

    Introduction to Android

---
+++ AndroidSession1/ex1/src/main/java/com/example/ex1/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public  void navigate(View view){
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        startActivity(intent);
+    }
+}
