bffbf0032d4bb90ad436a7201c47b439e9c026db
suboo <subo.ttn.com>
2020-07-06 17:24:40 +0530

    Sample project on Room Database

---
+++ RoomExample/app/build/tmp/kapt3/stubs/debug/com/example/roomexample/Room/Employee.java
@@ -0,0 +1,86 @@
+package com.example.roomexample.Room;
+
+import java.lang.System;
+
+@androidx.room.Entity()
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u000f\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0087\b\u0018\u00002\u00020\u0001B+\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\b\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u0012\b\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u0012\b\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\u0002\u0010\bJ\t\u0010\u000f\u001a\u00020\u0003H\u00c6\u0003J\u000b\u0010\u0010\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u0011\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u0012\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J7\u0010\u0013\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\n\b\u0002\u0010\u0004\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0007\u001a\u0004\u0018\u00010\u0005H\u00c6\u0001J\u0013\u0010\u0014\u001a\u00020\u00152\b\u0010\u0016\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0017\u001a\u00020\u0003H\u00d6\u0001J\t\u0010\u0018\u001a\u00020\u0005H\u00d6\u0001R\u0013\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u0013\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\nR\u0013\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\n\u00a8\u0006\u0019"}, d2 = {"Lcom/example/roomexample/Room/Employee;", "", "eId", "", "firstName", "", "lastName", "city", "(ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getCity", "()Ljava/lang/String;", "getEId", "()I", "getFirstName", "getLastName", "component1", "component2", "component3", "component4", "copy", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class Employee {
+    @androidx.room.PrimaryKey()
+    private final int eId = 0;
+    @org.jetbrains.annotations.Nullable()
+    private final java.lang.String firstName = null;
+    @org.jetbrains.annotations.Nullable()
+    private final java.lang.String lastName = null;
+    @org.jetbrains.annotations.Nullable()
+    private final java.lang.String city = null;
+    
+    public final int getEId() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getFirstName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getLastName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getCity() {
+        return null;
+    }
+    
+    public Employee(int eId, @org.jetbrains.annotations.Nullable()
+    java.lang.String firstName, @org.jetbrains.annotations.Nullable()
+    java.lang.String lastName, @org.jetbrains.annotations.Nullable()
+    java.lang.String city) {
+        super();
+    }
+    
+    public final int component1() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.roomexample.Room.Employee copy(int eId, @org.jetbrains.annotations.Nullable()
+    java.lang.String firstName, @org.jetbrains.annotations.Nullable()
+    java.lang.String lastName, @org.jetbrains.annotations.Nullable()
+    java.lang.String city) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object p0) {
+        return false;
+    }
+}
---
+++ RoomExample/app/build/tmp/kapt3/stubs/debug/com/example/roomexample/ViewModel/EmployeeViewModel.java
@@ -0,0 +1,30 @@
+package com.example.roomexample.ViewModel;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u00008\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\r\u001a\u00020\b2\u0006\u0010\u000e\u001a\u00020\u000fJ\u000e\u0010\u0010\u001a\u00020\u00112\u0006\u0010\u0012\u001a\u00020\bR\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u000e\u0010\u000b\u001a\u00020\fX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0013"}, d2 = {"Lcom/example/roomexample/ViewModel/EmployeeViewModel;", "Landroidx/lifecycle/AndroidViewModel;", "application", "Landroid/app/Application;", "(Landroid/app/Application;)V", "employeeList", "Landroidx/lifecycle/LiveData;", "", "Lcom/example/roomexample/Room/Employee;", "getEmployeeList", "()Landroidx/lifecycle/LiveData;", "repository", "Lcom/example/roomexample/Repository/Repository;", "getByName", "firstName", "", "insert", "", "employee", "app_debug"})
+public final class EmployeeViewModel extends androidx.lifecycle.AndroidViewModel {
+    private final com.example.roomexample.Repository.Repository repository = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.LiveData<java.util.List<com.example.roomexample.Room.Employee>> employeeList = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.util.List<com.example.roomexample.Room.Employee>> getEmployeeList() {
+        return null;
+    }
+    
+    public final void insert(@org.jetbrains.annotations.NotNull()
+    com.example.roomexample.Room.Employee employee) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.roomexample.Room.Employee getByName(@org.jetbrains.annotations.NotNull()
+    java.lang.String firstName) {
+        return null;
+    }
+    
+    public EmployeeViewModel(@org.jetbrains.annotations.NotNull()
+    android.app.Application application) {
+        super(null);
+    }
+}
